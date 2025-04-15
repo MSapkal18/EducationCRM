efec3826421ced8e766e88aebce7c306d2edb8ff
MSapkal18 <emrsmanish@gmail.com>
2025-04-15 23:18:05 +0530

    start

--- src/main/java/in/sp/main/controllers/VideoController.java
+++ src/main/java/in/sp/main/controllers/VideoController.java
@@ -1,8 +1,14 @@
 package in.sp.main.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VideoController
 {
+    @GetMapping
+    public String videoStart()
+    {
+        return "";
+    }
 }
